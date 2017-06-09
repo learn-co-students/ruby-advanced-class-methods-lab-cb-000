5f6152f5025c2adb160f44a805d5cde4b3a4b203
Tony Nassif <tonynassif@gmail.com>
2017-06-09 19:51:42 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -4,14 +4,21 @@ class Song
 
   def self.create
     song = self.new
-    @@all << song
+    all << song
     song
   end
 
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    @@all << song
+    all << song
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    all << song
     song
   end
 
