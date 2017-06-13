9b5cd9cd337164f7e31659ca2f54fa64df2d4919
Robert Downey <countrysc@yahoo.com>
2017-06-13 01:16:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,67 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.all.include?(name)
+      return name
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    alph_array = self.all.sort {|a, b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(mp3)
+    data = mp3.split(" - ")
+    artist_name = data[0]
+    name = data[1].chomp(".mp3")
+
+    song = self.new
+
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(mp3)
+    data = mp3.split(" - ")
+    artist_name = data[0]
+    name = data[1].chomp(".mp3")
+
+    song = self.new
+
+    song.name = name
+    song.artist_name = artist_name
+    @@all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
