3533030464f0d7f17d8dcfb58c89c118d6b1e17f
Mark Fowler <fowlermark488@gmail.com>
2017-11-19 01:44:11 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -12,50 +12,54 @@ class Song
 
   def self.create
     song = self.new
-    self.all << song
+    song.save
     song
   end
 
-  def self.new_by_name(song_name)
+  def self.new_by_name(name)
     song = self.new
-    song.name = song_name
+    song.name = name
     song
   end
 
-def self.create_by_name(song_name)
-  song = self.new
-  song.name = song_name
-  self.all << song
-  song
-end
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
 
-def self.find_by_name(song_name)
-  self.all.detect{ |song| song.name == song_name }
-end
+  def self.find_by_name(name)
+    self.all.detect { |song| song.name == name }
+  end
 
-def self.find_or_create_by_name(song_name)
-  self.find_by_name(song_name) || self.create_by_name(song_name)
-end
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
 
-def self.alphabetical
-  self.all.sort { |a,b| a.name <=> b.name }
-end
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
 
-def self.new_from_filename(filename)
-  edited_filename = filename.chomp(".mp3").split(" - ")
-  song = self.new
-  song.name = edited_filename[1]
-  song.artist_name = edited_filename[0]
-  song
-end
+  def self.new_from_filename(filename)
+    song_and_artist = filename.chomp(".mp3")
+    song_and_artist_parsed = song_and_artist.split(" - ")
+    artist = song_and_artist_parsed[0]
+    song_title = song_and_artist_parsed[1]
 
-def self.create_from_filename(filename)
-  song = self.all << self.new_from_filename(filename)
-  song
-end
+    new_song = self.new
+    new_song.name = song_title
+    new_song.artist_name = artist
+    new_song
+  end
 
-def self.destroy_all
-  self.all.clear
-end
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 
 end
