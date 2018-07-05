f06a262755710ba2936227303c0000c1e580f652
Tao <werewaswas@live.com>
2018-07-05 23:15:36 +0000

    Done.

--- README.md
+++ README.md
@@ -15,6 +15,17 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+  end
+
+  def self.create(name, artist_name)
+    song = self.new(name, artist_name)
+    @@all << song
+    return song
+  end
+
   def self.all
     @@all
   end
--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,48 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.all
-    @@all
+  def initialize(name=" ", artist_name=" ")
+    @name = name
+    @artist_name = artist_name
+    @@all << self
+  end
+
+  def self.create
+    song = self.new
   end
 
   def save
     self.class.all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_name(name)
+    self.new(name)
+  end
+
+  def self.create_by_name(name)
+    self.new(name)
+  end
+
+def self.find_by_name(songname)
+  @@all.find{|song| song.name == songname}
+end
+
+
+def self.find_or_create_by_name(songname)
+  if self.find_by_name(songname)
+    self.find_by_name(songname)
+  else
+    self.create_by_name(songname)
+  end
+end
+
+def self.destroy_all
+  @@all.clear
+end
+
 
 end
