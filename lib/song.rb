4e6b258af882e09135d88fb360a9e91ef213029d
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-21 06:51:19 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -45,5 +45,24 @@ class Song
     return self.all.sort_by{|song| song.name}
   end
 
+  def self.new_from_filename(filename)
+    artist_name = filename.split(" - ")[0]
+    song_name = filename.split(" - ")[1].split(".")[0]
+    new_song = self.find_or_create_by_name(song_name)
+    new_song.artist_name = artist_name
+    return new_song
+  end
+  
+  def self.create_from_filename(filename)
+    artist_name = filename.split(" - ")[0]
+    song_name = filename.split(" - ")[1].split(".")[0]
+    new_song = self.find_or_create_by_name(song_name)
+    new_song.artist_name = artist_name
+    return new_song
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
 
 end
